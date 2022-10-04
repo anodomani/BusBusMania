8fe6160318c1e81981e7d6273fc262c92034fcd5
Elicantoo <karinaa.arellano02@gmail.com>
2022-10-04 13:22:56 -0500

    UI art added and Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager Instance { get; private set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        foreach(Sound s in sounds)
+        {
+            s.source=gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Main_music");
+    }
+
+    // Update is called once per frame
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+    }
+}
